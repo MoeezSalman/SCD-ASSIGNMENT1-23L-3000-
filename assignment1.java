8bc4d5efd998b8d97e3ccdc96127e13b697fae70
Muhammad Moeez <moeezsalman246@gmail.com>
2025-09-13 23:42:58 +0500

    setting the changes in the overlapping thing

--- assignment1.java
+++ assignment1.java
@@ -2,120 +2,152 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.Date;
-class Task{                             // For the calculation of the number of tasks
 
-int id;
-String title;
-String startime;
-String endtime;
+class Task {
+    int id;
+    String title;
+    String startime;
+    String endtime;
+    int[] dependencies;
+    int depCount;
 
+    public Task(int id, String title, String start, String end) {
+        this.id = id;
+        this.title = title;
+        this.startime = start;
+        this.endtime = end;
+        this.dependencies = new int[10];
+        this.depCount = 0;
+    }
 
+    public void addDependency(int depId) {
+        if (depCount < dependencies.length) {
+            dependencies[depCount++] = depId;
+        }
+    }
 
-public Task(int id, String title , String start, String end){
+    public int getDurationHours() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd+HHmm");
+        try {
+            Date startDate = sdf.parse(startime);
+            Date endDate = sdf.parse(endtime);
+            long difference = endDate.getTime() - startDate.getTime();
+            return (int) (difference / (1000 * 60 * 60));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
-    this.id=id;
-    this.title=title;
-    this.startime=start;
-    this.endtime=end;
-   
-
-}
-
-
-public int getDurationHours() {
-    // Parse the date strings properly
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd+HHmm");
-    
-    try {
-        Date startDate = sdf.parse(startime);
-        Date endDate = sdf.parse(endtime);
-        
-        long difference = endDate.getTime() - startDate.getTime();
-        return (int) (difference / (1000 * 60 * 60)); // Convert milliseconds to hours
-    } catch (ParseException e) {
-        e.printStackTrace();
-        return 0;
+    @Override
+    public String toString() {
+        return id + " - " + title + " ( " + startime + " to " + endtime + " )";
     }
 }
 
-
-
-@Override
-public String toString(){
-    return id + " - "+title+" ( "+startime+ " to "+endtime+" )";
-}
-
-}
-
-
-
-class Resource{                          // For adding the resources from the resource.txt file
+class Resource {
     String name;
     int[] taskIds;
     int[] completedtaskamount;
     int count;
 
-    public Resource(String name, int maxTasks){
-        this.name=name;
-        this.taskIds=new int[maxTasks];
-        this.completedtaskamount= new int[maxTasks];
-        this.count=0;
+    public Resource(String name, int maxTasks) {
+        this.name = name;
+        this.taskIds = new int[maxTasks];
+        this.completedtaskamount = new int[maxTasks];
+        this.count = 0;
     }
 
-    public void assignTask(int taskId, int percentage){
-        taskIds[count]=taskId;
-        completedtaskamount[count]=percentage;
+    public void assignTask(int taskId, int percentage) {
+        taskIds[count] = taskId;
+        completedtaskamount[count] = percentage;
         count++;
     }
 }
 
-class Project{                          //For all the add resource,getting completion time and functionalities
-
+class Project {
     Task[] tasks;
     Resource[] resources;
     int taskcount;
     int resourcecount;
 
-    public Project(int maxtasks, int maxResources){
-        tasks=new Task[maxtasks];
-        resources= new Resource[maxResources];
-        taskcount=0;
-        resourcecount=0;
-
-
+    public Project(int maxtasks, int maxResources) {
+        tasks = new Task[maxtasks];
+        resources = new Resource[maxResources];
+        taskcount = 0;
+        resourcecount = 0;
     }
 
-    public void addTask(Task t){
-        tasks[taskcount++]=t;
+    public void addTask(Task t) {
+        tasks[taskcount++] = t;
     }
 
-    public void addResource(Resource r){
-        resources[resourcecount++]=r;
+    public void addResource(Resource r) {
+        resources[resourcecount++] = r;
     }
 
-    public String getcompletionTime(){
-        String max=tasks[0].endtime;
-        for(int i=1;i<taskcount;i++){
-            if(tasks[i].endtime.compareTo(max)>0){
-                max=tasks[i].endtime;
+    public String getcompletionTime() {
+        String max = tasks[0].endtime;
+        for (int i = 1; i < taskcount; i++) {
+            if (tasks[i].endtime.compareTo(max) > 0) {
+                max = tasks[i].endtime;
             }
         }
         return max;
     }
 
-    public void findoverlaps(){                             //Checking overlaps
-        for(int i=0;i<taskcount;i++){
-            for(int j=i+1;j<taskcount;j++){
-                Task t1=tasks[i], t2=tasks[j];
-                if (t1.endtime.compareTo(t2.startime) > 0 && t1.startime.compareTo(t2.endtime) < 0) {
-                    System.out.println("Overlap: " + t1.title + " & " + t2.title);
+    public void findDependencyOverlaps() {
+        
+        boolean found = false;
+        
+        for (int i = 0; i < taskcount; i++) {
+            Task currentTask = tasks[i];
+            
+            for (int j = 0; j < currentTask.depCount; j++) {
+                int depId = currentTask.dependencies[j];
+                Task dependency = findTask(depId);
+                
+                if (dependency != null) {
+                    // Check if current task starts before its dependency ends
+                    if (currentTask.startime.compareTo(dependency.endtime) < 0) {
+                        // Check if there's actual time overlap (not just touching)
+                        if (currentTask.endtime.compareTo(dependency.startime) > 0) {
+                            System.out.println("OVERLAP VIOLATION: " + currentTask.title + 
+                                             " starts before its dependency " + dependency.title + " is completed");
+                            
+                            found = true;
+                        }
+                    }
                 }
             }
         }
+        
+        if (!found) {
+            System.out.println("No dependency violations found.");
+        }
     }
 
+    private int calculateOverlapHours(Task task1, Task task2) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd+HHmm");
+        try {
+            Date start1 = sdf.parse(task1.startime);
+            Date end1 = sdf.parse(task1.endtime);
+            Date start2 = sdf.parse(task2.startime);
+            Date end2 = sdf.parse(task2.endtime);
+            
+            long overlapStart = Math.max(start1.getTime(), start2.getTime());
+            long overlapEnd = Math.min(end1.getTime(), end2.getTime());
+            
+            if (overlapStart < overlapEnd) {
+                return (int) ((overlapEnd - overlapStart) / (1000 * 60 * 60));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
-    public void getTeamForTask(int taskId) {                //Finding the name of the assigned task employee
+    public void getTeamForTask(int taskId) {
         System.out.print("Team for Task " + taskId + ": ");
         for (int i = 0; i < resourcecount; i++) {
             for (int j = 0; j < resources[i].count; j++) {
@@ -127,10 +159,7 @@ class Project{                          //For all the add resource,getting compl
         System.out.println();
     }
 
- 
-
-
-    public void calculateEffort() {                       //Getting effort of all the members
+    public void calculateEffort() {
         for (int i = 0; i < resourcecount; i++) {
             int total = 0;
             Resource r = resources[i];
@@ -146,45 +175,42 @@ class Project{                          //For all the add resource,getting compl
         }
     }
 
-   public Task findTask(int id) {
+    public Task findTask(int id) {
         for (int i = 0; i < taskcount; i++) {
             if (tasks[i].id == id) return tasks[i];
         }
         return null;
     }
-
 }
 
-
-
-
-public class assignment1{                           //Main branch
-    public static void main(String[] args){
-
-        try{
-            Project project= new Project(20, 10);
+public class assignment1 {
+    public static void main(String[] args) {
+        try {
+            Project project = new Project(20, 10);
             BufferedReader br1 = new BufferedReader(new FileReader("tasks.txt"));
             String line;
+            
             while ((line = br1.readLine()) != null) {
-
-                String[] parts=line.split(",");
+                String[] parts = line.split(",");
                 int id = Integer.parseInt(parts[0].trim());
                 String title = parts[1].trim();
                 String start = parts[2].trim();
                 String end = parts[3].trim();
                 
-
+                Task task = new Task(id, title, start, end);
                 
-
-                 project.addTask(new Task(id, title, start, end));
-
+                // Add dependencies if they exist
+                for (int i = 4; i < parts.length; i++) {
+                    int depId = Integer.parseInt(parts[i].trim());
+                    task.addDependency(depId);
+                }
+                
+                project.addTask(task);
             }
-             br1.close();
+            br1.close();
 
-
-             BufferedReader br2 = new BufferedReader(new FileReader("resources.txt"));
-             while ((line = br2.readLine()) != null) {
-
+            BufferedReader br2 = new BufferedReader(new FileReader("resources.txt"));
+            while ((line = br2.readLine()) != null) {
                 String[] parts = line.split(",");
                 Resource r = new Resource(parts[0].trim(), 20);
                 for (int i = 1; i < parts.length; i++) {
@@ -194,20 +220,17 @@ public class assignment1{                           //Main branch
                     r.assignTask(taskId, percentage);
                 }
                 project.addResource(r);
-
-             }
-             br2.close();
-             System.out.println("Project completion time: " + project.getcompletionTime());
-            project.findoverlaps();
+            }
+            br2.close();
+            
+            // Project completion time output
+            System.out.println("Project completion time: " + project.getcompletionTime());
+            project.findDependencyOverlaps();
             project.getTeamForTask(3);
             project.calculateEffort();
-        }
-        catch(Exception e){
+            
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
     }
 }
-
